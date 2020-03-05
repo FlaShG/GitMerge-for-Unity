4e501ced233805faaaf8819e8d433108cd8c6b35
Arnaud Begue <abegue@persistant.fr>
2020-03-05 12:07:59 -0500

    Add a command bar to select 'use theirs' and 'use ours' on all actions + Add a filter bar

---
+++ Editor/MergeFilter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class MergeFilter
+{
+    public enum FilterMode
+    {
+        Inclusion,
+        Exclusion
+    }
+
+    public bool useFilter { get; set; } = false;
+    public bool isRegex { get; set; } = false;
+    public bool isCaseSensitive { get; set; } = false;
+
+    private string _expression = string.Empty;
+    public string expression
+    {
+        get => _expression;
+        set
+        {
+            if (_expression != value)
+            {
+                _expression = value;
+                regex = new Regex(_expression);
+            }
+        }
+    }
+
+    public FilterMode filterMode { get; set; } = FilterMode.Inclusion;
+
+    private Regex regex = new Regex(string.Empty);
+
+    public bool IsPassingFilter(string input)
+    {
+        if (!useFilter)
+        {
+            return true;
+        }
+
+        bool isPassingFilter = false;
+
+        if (isRegex)
+        {
+            isPassingFilter = regex.IsMatch(input);
+        }
+        else
+        {
+            if (!isCaseSensitive)
+            {
+                input = input.ToLowerInvariant();
+                isPassingFilter = input.Contains(_expression.ToLowerInvariant());
+            }
+            else
+            {
+                isPassingFilter = input.Contains(_expression);
+            }
+        }
+
+        if (filterMode == FilterMode.Exclusion)
+        {
+            isPassingFilter = !isPassingFilter;
+        }
+
+        return isPassingFilter;
+    }
+}
